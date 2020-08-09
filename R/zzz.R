864cc874b8aea7f97a282f300f70c08e40666d25
Vinay Udyawer <vinay.udyawer@gmail.com>
2020-08-09 21:10:39 +0930

    update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,6 @@
 ## Processing functions on startup of package
 
 .onLoad <- function(libname, pkgname){
-  invisible(suppressPackageStartupMessages(
-    sapply(c("tibble", "purrr", "dplyr", "tidyr", "ggplot2", "data.table", "adehabitatMA", "foreach", "parallel", "doParallel"),
-           requireNamespace, quietly = TRUE)
-  ))
   ver <- utils::packageVersion("VTrack")
   packageStartupMessage("Loading VTrack version ", ver)
 }
