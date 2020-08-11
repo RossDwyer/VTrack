911544df72cac0b8f6e153f930b18fca77dbaedd
Vinay Udyawer <vinay.udyawer@gmail.com>
2020-08-11 12:08:23 +0930

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,17 @@
 ## Processing functions on startup of package
 
 .onAttach <- function(libname, pkgname){
-  ver <- utils::packageVersion("VTrack")
+  ver <- as.numeric(utils::packageDescription("Vtrack", fields = c("Version")))
   packageStartupMessage("VTrack version ", ver, " loaded")
   
   ## Check if an updated version of VTrack is available on GitHub
   github_url <- readLines("https://raw.githubusercontent.com/rossdwyer/VTrack/master/DESCRIPTION")
-  github_ver <- gsub("Version:\\s*", "", github_url[grep("Version:", x)])
+  github_ver <- as.numeric(gsub("Version:\\s*", "", github_url[grep("Version:", x)]))
   
   if(github_ver > ver){
-    packageStartupMessage(paste0("Updated version of VTrack available on GitHub [ver ", github_ver,"]",
-                                 "\ndownload new version using\n",
+    packageStartupMessage(paste0("===========================================================",
+                                 "\nUpdated version of VTrack available on GitHub [ver ", github_ver,"]",
+                                 "\ndownload new version using:\n",
                                  "devtools::install_github('rossdwyer/VTrack')"))
   }
 }
